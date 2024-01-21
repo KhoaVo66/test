a9df7f56a86bd05ff8bc5296a0cab5049dd3f175
ACER\khoav <khoa93076@donga.edu.vn>
2024-01-21 10:57:30 +0700

    aaa

--- src/components/pages/admin/edit_product.jsx
+++ src/components/pages/admin/edit_product.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, Link} from "react-router-dom";
 
 export default function EditProduct() {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const { id } = useParams();
   const [productName, setProductName] = useState();
   const [image, setImage] = useState();
