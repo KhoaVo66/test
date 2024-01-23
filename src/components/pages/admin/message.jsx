e594032ac348a9c5f0c3c9a4d0f625ab2ec64c6e
ACER\khoav <khoa93076@donga.edu.vn>
2024-01-23 20:15:03 +0700

    update read feedback

--- src/components/pages/admin/message.jsx
+++ src/components/pages/admin/message.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Message = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://web-shopping-exclusive.onrender.com/feedback")
+      .then((result) => {
+        setData(result);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div className="container">
       <div className="wrapper">
@@ -17,7 +29,6 @@ const Message = () => {
             <table>
               <thead>
                 <tr>
-                  <th>Id</th>
                   <th>Name</th>
                   <th>Email</th>
                   <th>Phone</th>
@@ -25,20 +36,14 @@ const Message = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td className="cart-text">1</td>
-                  <td className="cart-text">Nguyen Tuan Anh</td>
-                  <td className="cart-text">anh123@gmail.com</td>
-                  <td className="cart-text">0123456789</td>
-                  <td className="cart-text">Web lua dao a</td>
-                </tr>
-                <tr>
-                  <td className="cart-text">2</td>
-                  <td className="cart-text">Nguyen Huu Hiep</td>
-                  <td className="cart-text">soicodoc@gmail.com</td>
-                  <td className="cart-text">0123456789</td>
-                  <td className="cart-text">San pham tuyet voi toi rat thich</td>
-                </tr>
+                {data.map((f, i) => (
+                  <tr key={i}>
+                    <td className="cart-text">{f.name}</td>
+                    <td className="cart-text">{f.email}</td>
+                    <td className="cart-text">{f.phone_number}</td>
+                    <td className="cart-text">{f.message}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
